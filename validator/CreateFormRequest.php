0b4dd705a2ac89aeb6e14b36c295f16d8e69786c
kyawlinntun <kyawlinntun100@gmail.com>
2024-12-04 11:33:33 +0630

    remove print_r

--- validator/CreateFormRequest.php
+++ validator/CreateFormRequest.php
@@ -119,6 +119,5 @@ if ($validator->validate()) {
     $errors = $validator->errors();
     $_SESSION['data'] = json_encode($data);
     $_SESSION['errors'] = json_encode($errors);
-    // print_r($errors);
     $router->redirect('/');
 }

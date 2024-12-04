689ae7aef3c196d1a0f3c0bb81b7f587a91948b9
kyawlinntun <kyawlinntun100@gmail.com>
2024-12-04 15:06:38 +0630

    Change env

--- env.php
+++ env.php
@@ -2,8 +2,8 @@
 
 define('VIEW_PATH', __DIR__ . '/view/');
 define('VIEW', __DIR__ . '/view/');
-define('VALIDATOR', __DIR__ . '/validator/');
+define('VALIDATOR', __DIR__ . '/controller/validator/');
 define('ROUTES', __DIR__ . '/routes/');
 define('MODEL', __DIR__ . '/model/');
 define('CONTROLLER', __DIR__ . '/controller/');
-define('REQUEST', __DIR__ . '/request/');
+define('REQUEST', __DIR__ . '/controller/request/');

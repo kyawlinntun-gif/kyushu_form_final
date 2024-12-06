de629162e0bf109d873f9447bea39fb6ecb3c2ff
kyawlinntun <kyawlinntun100@gmail.com>
2024-12-06 10:18:56 +0630

    final check

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function view($path, $options=[]) {
     include VIEW_PATH . $path;
 }
 function prepareToStore($input){
-    return htmlspecialchars(preg_replace('/\s+/', ' ', $input), ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
+    return htmlspecialchars(preg_replace('/\s+/', ' ', trim($input)), ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
 }
 function normalize($input){
     return array_map('prepareToStore',$input);
